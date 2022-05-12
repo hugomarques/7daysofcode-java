7dff7af21cfa0de339a4b691c4c7fe803ffb8a82
Hugo Marques <hugom@twitter.com>
2022-05-11 20:08:58 -0700

    (feat): Dia 2
    A tarefa hoje é extrair as informações do JSON. Normalmente fazemos isso com uma lib como Jackson ou gson,
    mas para o intuito do desafio vamos fazer na mão usando regex T___T.

--- src/com/hugaomarques/Top250Imdb.java
+++ src/com/hugaomarques/Top250Imdb.java
@@ -6,6 +6,11 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Top250Imdb {
 
@@ -24,6 +29,86 @@ public class Top250Imdb {
 
         final var httpClient = HttpClient.newHttpClient();
         final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        final var movies = parseContentJson(response.body());
+        final var ids = new ArrayList<String>();
+        final var ranks = new ArrayList<String>();
+        final var titles = new ArrayList<String>();
+        final var fullTitles = new ArrayList<String>();
+        final var years = new ArrayList<String>();
+        final var images = new ArrayList<String>();
+        final var crews = new ArrayList<List<String>>();
+        final var imdbRatingsCount = new ArrayList<String>();
+        movies.stream().forEach(
+            movie -> {
+                ids.add(parseId(movie));
+                ranks.add(parseRank(movie));
+                titles.add(parseTitle(movie));
+                fullTitles.add(parseFullTitle(movie));
+                years.add(parseYears(movie));
+                images.add(parseImages(movie));
+                crews.add(parseCrew(movie));
+                imdbRatingsCount.add(parseImdbRatingCount(movie));
+            }
+        );
+
+        System.out.println(movies);
+        System.out.println(ids);
+        System.out.println(ranks);
+        System.out.println(titles);
+        System.out.println(fullTitles);
+        System.out.println(years);
+        System.out.println(images);
+        System.out.println(crews);
+        System.out.println(imdbRatingsCount);
+
+    }
+
+    private static String parseImdbRatingCount(String movie) {
+        return parseAttribute("imDbRatingCount", movie);
+    }
+
+    private static List<String> parseCrew(String movie) {
+        return Arrays.asList(parseAttribute("crew", movie).split(","));
+    }
+
+    private static String parseImages(String movie) {
+        return parseAttribute("image", movie);
+    }
+
+    private static String parseYears(String movie) {
+        return parseAttribute("year", movie);
+    }
+
+    private static String parseFullTitle(String movie) {
+        return parseAttribute("fullTitle", movie);
+    }
+
+    private static String parseTitle(String movie) {
+        return parseAttribute("title", movie);
+    }
+
+    private static String parseRank(String movie) {
+        return parseAttribute("rank", movie);
+    }
+
+    private static String parseId(String movie) {
+        return parseAttribute("id", movie);
+    }
+
+    private static String parseAttribute(String attribute, String movie) {
+        var matcher = Pattern.compile("\""+attribute+"\":\"(.*?)\"").matcher(movie);
+        matcher.find();
+        return matcher.group(1);
+    }
+
+    private static List<String> parseContentJson(String json) {
+        final var contentMatcher = Pattern.compile("\\[(.*?)\\]").matcher(json);
+        if (!contentMatcher.find()) {
+            throw new IllegalStateException("Data on wrong format");
+        }
+        var content = contentMatcher.group(0);
+        final var moviesMatcher = Pattern.compile("\\{(.*?)\\}").matcher(content);
+        var groupIndex = 0;
+        return moviesMatcher.results().map(result -> result.group()).collect(Collectors.toList());
     }
 }
