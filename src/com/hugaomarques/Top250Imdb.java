d42ab526efab883d9f4fedefa11f39047a38f2d4
Hugo Marques <hugom@twitter.com>
2022-05-12 16:39:07 -0700

    (feat): Dia 3
    Converter o nosso código procedural para uma versão Orientada à Objetos.

--- src/com/hugaomarques/Top250Imdb.java
+++ src/com/hugaomarques/Top250Imdb.java
@@ -29,76 +29,11 @@ public class Top250Imdb {
 
         final var httpClient = HttpClient.newHttpClient();
         final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        final var movies = parseContentJson(response.body());
-        final var ids = new ArrayList<String>();
-        final var ranks = new ArrayList<String>();
-        final var titles = new ArrayList<String>();
-        final var fullTitles = new ArrayList<String>();
-        final var years = new ArrayList<String>();
-        final var images = new ArrayList<String>();
-        final var crews = new ArrayList<List<String>>();
-        final var imdbRatingsCount = new ArrayList<String>();
-        movies.stream().forEach(
-            movie -> {
-                ids.add(parseId(movie));
-                ranks.add(parseRank(movie));
-                titles.add(parseTitle(movie));
-                fullTitles.add(parseFullTitle(movie));
-                years.add(parseYears(movie));
-                images.add(parseImages(movie));
-                crews.add(parseCrew(movie));
-                imdbRatingsCount.add(parseImdbRatingCount(movie));
-            }
-        );
-
+        final var moviesJson = parseContentJson(response.body());
+        final var movies = moviesJson.stream()
+            .map(Movie::instanceOf)
+            .collect(Collectors.toList());
         System.out.println(movies);
-        System.out.println(ids);
-        System.out.println(ranks);
-        System.out.println(titles);
-        System.out.println(fullTitles);
-        System.out.println(years);
-        System.out.println(images);
-        System.out.println(crews);
-        System.out.println(imdbRatingsCount);
-
-    }
-
-    private static String parseImdbRatingCount(String movie) {
-        return parseAttribute("imDbRatingCount", movie);
-    }
-
-    private static List<String> parseCrew(String movie) {
-        return Arrays.asList(parseAttribute("crew", movie).split(","));
-    }
-
-    private static String parseImages(String movie) {
-        return parseAttribute("image", movie);
-    }
-
-    private static String parseYears(String movie) {
-        return parseAttribute("year", movie);
-    }
-
-    private static String parseFullTitle(String movie) {
-        return parseAttribute("fullTitle", movie);
-    }
-
-    private static String parseTitle(String movie) {
-        return parseAttribute("title", movie);
-    }
-
-    private static String parseRank(String movie) {
-        return parseAttribute("rank", movie);
-    }
-
-    private static String parseId(String movie) {
-        return parseAttribute("id", movie);
-    }
-
-    private static String parseAttribute(String attribute, String movie) {
-        var matcher = Pattern.compile("\""+attribute+"\":\"(.*?)\"").matcher(movie);
-        matcher.find();
-        return matcher.group(1);
     }
 
     private static List<String> parseContentJson(String json) {
